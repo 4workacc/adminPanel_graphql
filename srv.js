af492857dd585103927c4f4b4323b223a61b9e33
Eugen <58693140+4workacc@users.noreply.github.com>
2021-04-21 13:55:09 +0300

    simple qraphql serv

---
+++ srv.js
@@ -0,0 +1,25 @@
+let express = require("express");
+let { graphqlHTTP } = require("express-graphql");
+let { buildSchema } = require("graphql");
+
+let schema = buildSchema(`
+    type Query {
+        id: Int
+    }
+`);
+
+let root = {
+    id : -10
+};
+
+let app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema:schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(5000, ()=>{});
+
+
